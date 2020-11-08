4a40c08e1a68caba22d777d5c34e60c8601e5975
angel bello <angelsebastianbellomateo@gmail.com>
2020-11-08 12:39:59 -0400

    Base de datos creada en el app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+(function(){
+    
+    let DB;
+    const indexedDB = window.indexedDB;
 
+    document.addEventListener('DOMContentLoaded', crearDB);
 
-console.log('hola')
+    function crearDB(){
+        const request = indexedDB.open('crm', 1);
+
+        request.onsuccess = function(){
+            console.log('Acceso a DataBase');
+            DB = request.result
+        }
+
+        request.onupgradeneeded = function(){
+            console.log('DataBase creada/actualizada');
+            DB = request.result;
+            //Creamos el almacen de datos
+            const objectStore = DB.createObjectStore('crm', {
+                keyPath: 'id',
+                autoIncrement: true
+            })
+
+            //Creamos los index del almacen de datos
+            objectStore.createIndex('nombre', 'nombre', {unique: false});
+            objectStore.createIndex('email', 'email', {unique: true});
+            objectStore.createIndex('telefono', 'telefono', {unique: true});
+            objectStore.createIndex('empresa', 'empresa', {unique: false});
+            objectStore.createIndex('id', 'id', {unique: true});
+            
+        }
+
+        request. onerror = function(){
+            console.log('Hubo un error en el DataBase');
+        }
+    }
+
+})()
